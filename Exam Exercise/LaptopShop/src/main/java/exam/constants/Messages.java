4c280176eb83c802f30d1ebcb4a359572ccd2756
Valentin Tsonevski <tsonevski99@gmail.com>
2023-01-20 21:57:53 +0000

    Add files via upload

---
+++ Exam Exercise/LaptopShop/src/main/java/exam/constants/Messages.java
@@ -0,0 +1,13 @@
+package exam.constants;
+
+public enum Messages {
+    ;
+    public static String INVALID_TOWN = "Invalid town";
+    public static String SUCCESSFULLY_IMPORTED_TOWN_FORMAT = "Successfully imported Town %s";
+    public static String INVALID_SHOP = "Invalid shop";
+    public static String SUCCESSFULLY_IMPORTED_SHOP_FORMAT = "Successfully imported Shop %s - %s";
+    public static String INVALID_CUSTOMER = "Invalid customer";
+    public static String SUCCESSFULLY_IMPORTED_CUSTOMER_FORMAT = "Successfully imported Customer %s %s - %s";
+    public static String INVALID_LAPTOP = "Invalid laptop";
+    public static String SUCCESSFULLY_IMPORTED_LAPTOP_FORMAT = "Successfully imported Laptop %s - %.2f - %d - %d";
+}
